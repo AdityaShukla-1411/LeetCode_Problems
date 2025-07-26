559c587dc4eaea47346cd0284a847e916e204ba7
Aditya Shukla <128830035+AdityaShukla-1411@users.noreply.github.com>
2025-07-26 13:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (5.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,41 +1,40 @@
-from copy import deepcopy
 from collections import deque
-from typing import List
-
+from typing import List  # <--- ADD THIS LINE
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows = len(grid)
-        cols = len(grid[0])
-        grid_copy = deepcopy(grid)
+        if rows == 0:
+            return -1
 
+        cols = len(grid[0])
         fresh_cnt = 0
-        queue = deque()
+        rotten = deque()
 
         for r in range(rows):
             for c in range(cols):
-                if grid_copy[r][c] == 2:
-                    queue.append((r, c))
-                elif grid_copy[r][c] == 1:
+                if grid[r][c] == 2:
+                    rotten.append((r, c))
+                elif grid[r][c] == 1:
                     fresh_cnt += 1
-                    
-        minutes = 0
-        
-        while len(queue) != 0 and fresh_cnt > 0:
-            minutes += 1
-            total_rotten = len(queue)
-            for _ in range(total_rotten):
-                i, j = queue.popleft()
+
+        minutes_passed = 0
+
+        while rotten and fresh_cnt > 0:
+            minutes_passed += 1
+
+            for _ in range(len(rotten)):
+                x, y = rotten.popleft()
+
                 for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                    new_i, new_j = i + dx, j + dy
-                    if new_i < 0 or new_i == rows or new_j < 0 or new_j == cols:
+                    xx, yy = x + dx, y + dy
+                    if xx < 0 or xx == rows or yy < 0 or yy == cols:
                         continue
-                    if grid_copy[new_i][new_j] == 0 or grid_copy[new_i][new_j] == 2:
+                    if grid[xx][yy] == 0 or grid[xx][yy] == 2:
                         continue
+
                     fresh_cnt -= 1
-                    grid_copy[new_i][new_j] = 2
-                    queue.append((new_i, new_j))
-                    
-        if fresh_cnt > 0:
-            return -1
-        return minutes
+                    grid[xx][yy] = 2
+                    rotten.append((xx, yy))
+
+        return minutes_passed if fresh_cnt == 0 else -1
